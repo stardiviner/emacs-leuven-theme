11cb14c04d045d87bfd2b1308d10874ccc9a3796
stardiviner <numbchild@gmail.com>
2019-12-28 22:50:53 +0800

    change hl-line face color from "light blue" to "light green"

--- leuven-theme.el
+++ leuven-theme.el
@@ -4,7 +4,7 @@
 
 ;; Author: Fabrice Niessen <(concat "fniessen" at-sign "pirilampo.org")>
 ;; URL: https://github.com/fniessen/emacs-leuven-theme
-;; Version: 20191225.1555
+;; Version: 20191228.2250
 ;; Keywords: color theme
 
 ;; This file is part of GNU Emacs.
@@ -622,7 +622,7 @@ more...")
    `(highlight-changes ((,class (:foreground nil)))) ;; blue "#2E08B5"
    `(highlight-changes-delete ((,class (:strike-through nil :foreground nil)))) ;; red "#B5082E"
    `(highlight-symbol-face ((,class (:background "#FFFFA0"))))
-   `(hl-line ((,class (:background "#F6FECD" :extend t)))) ; Highlight current line.
+   `(hl-line ((,class (:background "#C7ECC1" :extend t)))) ; Highlight current line.
    `(hl-tags-face ((,class ,highlight-current-tag))) ; ~ Pair highlighting (matching tags).
    `(hl-sexp-face ((,class (:background "#DDDDDD" :extend t))))
    `(holiday-face ((,class (:foreground "#777777" :background "#E4EBFE"))))
